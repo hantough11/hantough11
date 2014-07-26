46235a0afd0e21662321ab49b372f5ec0c2e329d
hantough11 <hantough11@gmail.com>
2014-07-26 19:09:07 +0900

    Revert "Init"
    This reverts commit e8743e6f30bc8033d7b62dc6eea1dc0293d80c58.

--- websrc/php/login.php
+++
@@ -1,5 +0,0 @@
-<?php	
-	
-	require_once("../../html/login.html");
-
-?>
--- websrc/php/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include 'get_user_email.php';
-	
-	require_once("../../html/main.html");
-	
-	echo("<script language='javascript'>php_update_user_email(\"$user_email\");</script>");
-?>
